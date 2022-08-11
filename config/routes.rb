cd523fe4f0b39a8f8ad8b1f01bab62f4f9727cb7
cesar banchio <cbanchio5@gmail.com>
2022-08-11 15:34:03 +0200

    added posts model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'about', to: 'pages#about', as: :about
-  get 'contact',         to: 'pages#contact',    as: :contact
+  get 'contact', to: 'pages#contact', as: :contact
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_11_113741) do
+ActiveRecord::Schema.define(version: 2022_08_11_131647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 2022_08_11_113741) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_dogs_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.bigint "dog_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["dog_id"], name: "index_posts_on_dog_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +52,6 @@ ActiveRecord::Schema.define(version: 2022_08_11_113741) do
   end
 
   add_foreign_key "dogs", "users"
+  add_foreign_key "posts", "dogs"
+  add_foreign_key "posts", "users"
 end
