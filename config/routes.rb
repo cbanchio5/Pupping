9c8af07813acd14a277af0cb7c687a67cfafaab6
cesar banchio <cbanchio5@gmail.com>
2022-08-16 17:20:55 +0200

    solved rotes problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users do
-    resources :dogs
-  end
   root to: 'pages#home'
   get 'about', to: 'pages#about', as: :about
   get 'contact', to: 'pages#contact', as: :contact
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts
+  devise_for :users
+  resource :users do
+    resources :dogs
+  end
+
 end
